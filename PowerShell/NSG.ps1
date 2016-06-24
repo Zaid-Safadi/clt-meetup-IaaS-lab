adc02c94f218b18c6ee0c0fec80a285dd5878733
Xeos <Zaid-Safadi@users.noreply.github.com>
2016-06-24 00:35:27 -0400

    Initial Commit

---
+++ PowerShell/NSG.ps1
@@ -0,0 +1,46 @@
+﻿#Create Netowrk Security Group and apply it to VNet with two subnets to model a DMZ/Backend network environment 
+
+Login-AzureRmAccount
+$ResourceGroupName = "clt-meetup-resg"
+$VNetName = "clt-meetup-vnet"
+$NSGName = "clt-meetup-nsg"
+$DeploymentLocation = "eastus2"
+$FrontendAddress = "192.168.1.0/24"
+$BackendAddress = "192.168.2.0/24"
+
+
+#Allow RDP to VMs on the entire VNet
+$rdpRule = New-AzureRmNetworkSecurityRuleConfig -Name "Allow-RDP" -Direction Inbound -Priority 110 -Access Allow -SourceAddressPrefix INTERNET -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange '3389' -Protocol Tcp -Description "Enable RDP to $VNetName VNet"
+
+#Allow incoming http internet traffic on port 80 to the forntend subnet
+$httpRule = New-AzureRmNetworkSecurityRuleConfig -Name "Allow-HTTP" -Direction Inbound -Priority 120 -Access Allow -SourceAddressPrefix INTERNET -SourcePortRange * -DestinationAddressPrefix $FrontendAddress -DestinationPortRange '80' -Protocol Tcp -Description "Enable HTTP to $VNetName VNet"
+
+#Allow secure https incoming internet traffic on port 443 to the forntend subnet
+$httpsRule = New-AzureRmNetworkSecurityRuleConfig -Name "Allow-HTTPs" -Direction Inbound -Priority 130 -Access Allow -SourceAddressPrefix INTERNET -SourcePortRange * -DestinationAddressPrefix $FrontendAddress -DestinationPortRange '443' -Protocol Tcp -Description "Enable HTTPs to $VNetName VNet"
+
+#Allow traffic from the Frontend subnet to the backend subnet on port 3306 only (database)
+$allowFrontToBackRule = New-AzureRmNetworkSecurityRuleConfig -Name "Allow-Database-backend " -Direction Inbound -Priority 140 -Access Allow -SourceAddressPrefix $FrontendAddress -SourcePortRange * -DestinationAddressPrefix $BackendAddress -DestinationPortRange '3306' -Protocol Tcp -Description "Enable MySQL DB to backend subnet from dmz"
+
+#Deny all other incoming traffic from the internet on the entire VNet
+$denyInternetRule = New-AzureRmNetworkSecurityRuleConfig -Name "Deny-Internet-External" -Direction Inbound -Priority 210 -Access Deny -SourceAddressPrefix INTERNET -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange * -Protocol * -Description "Isolate the $VNetName VNet from the Internet"
+
+#Deny all other incoming traffic from the frontend to the backend subnet
+$denyFrontToBackRule = New-AzureRmNetworkSecurityRuleConfig -Name "Deny-Frontend-Backend" -Direction Outbound -Priority 220 -Access Deny -SourceAddressPrefix $FrontendAddress -SourcePortRange * -DestinationAddressPrefix $BackendAddress -DestinationPortRange * -Protocol * -Description "Deny Frontend clssubnet access to Backend subnet"
+
+#Dent internet on the backend subnet (prevent internet browsing and downloading)    
+$denyInternetFromBackend = New-AzureRmNetworkSecurityRuleConfig -Name "Deny-Backend-Internet" -Direction Outbound -Priority 200 -Access Deny -SourceAddressPrefix $BackendAddress -SourcePortRange * -DestinationAddressPrefix Internet -DestinationPortRange * -Protocol * -Description "Block Internet"
+
+#Create new NSG
+$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Name $NSGName -Location $DeploymentLocation -SecurityRules $rdpRule,$httpRule,$httpsRule,$denyInternetRule,$denyFrontToBackRule,$denyInternetFromBackend
+
+#Get exsiting VNet
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $VNetName 
+
+#Apply NSG on the frontend subnet
+Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name Frontend -NetworkSecurityGroup $nsg -AddressPrefix $FrontendAddress
+
+#Apply NSG on the backend subnet
+Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name Backend -NetworkSecurityGroup $nsg -AddressPrefix $BackendAddress
+
+#Apply changes to the VNet
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
---
+++ PowerShell/VMs.ps1
@@ -0,0 +1,48 @@
+﻿Login-AzureRmAccount
+
+# Set values for existing resource group and storage account names
+$rgName="clt-meetup-resg"
+$locName="eastus2"
+$saName="clitmeetupsto"
+
+# Set the existing virtual network and subnet index
+$vnetName="clt-meetup-vnet"
+$subnetIndex=1
+
+$vnet=Get-AzureRMVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
+
+################
+# Create the NIC
+$nicName="example-win-vm-NIC"
+$domName="example-win-vm-xyz"
+$pip=New-AzureRmPublicIpAddress -Name $nicName -ResourceGroupName $rgName -DomainNameLabel $domName -Location $locName -AllocationMethod Dynamic
+$nic=New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $locName -SubnetId $vnet.Subnets[$subnetIndex].Id -PublicIpAddressId $pip.Id
+
+# Specify the name and size
+$vmName="example-win-vm"
+$vmSize="Standard_A0"
+$vm=New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize
+
+# Add a 50 GB additional data disk
+$diskSize=50
+$diskLabel="APPStorage"
+$diskName="21050529-DISK02"
+$storageAcc=Get-AzureRmStorageAccount -ResourceGroupName $rgName -Name $saName
+$vhdURI=$storageAcc.PrimaryEndpoints.Blob.ToString() + "vhds/" + $vmName + $diskName  + ".vhd"
+Add-AzureRmVMDataDisk -VM $vm -Name $diskLabel -DiskSizeInGB $diskSize -VhdUri $vhdURI -CreateOption empty
+
+# Specify the image and local administrator account, and then add the NIC
+$pubName="MicrosoftWindowsServer"
+$offerName="WindowsServer"
+$skuName="2012-R2-Datacenter"
+$cred=Get-Credential -Message "Type the name and password of the local administrator account."
+$vm=Set-AzureRmVMOperatingSystem -VM $vm -Windows -ComputerName $vmName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+$vm=Set-AzureRmVMSourceImage -VM $vm -PublisherName $pubName -Offer $offerName -Skus $skuName -Version "latest"
+$vm=Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+
+# Specify the OS disk name and create the VM
+$diskName="OSDisk"
+$storageAcc=Get-AzureRmStorageAccount -ResourceGroupName $rgName -Name $saName
+$osDiskUri=$storageAcc.PrimaryEndpoints.Blob.ToString() + "vhds/" + $vmName + $diskName  + ".vhd"
+$vm=Set-AzureRmVMOSDisk -VM $vm -Name $diskName -VhdUri $osDiskUri -CreateOption fromImage
+New-AzureRmVM -ResourceGroupName $rgName -Location $locName -VM $vm
